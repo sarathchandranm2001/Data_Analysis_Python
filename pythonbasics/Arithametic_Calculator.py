0543954993d97c6e8898c1c3e9745cdda6c85cc3
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-06-10 14:45:33 +0530

    Conditional Statements :

---
+++ pythonbasics/Arithametic_Calculator.py
@@ -0,0 +1,20 @@
+a=int(input("Enter First Number :"))
+b=int(input("Enter Second Number:"))
+opr=input("Enter Operation : (+,-,*,/,%)")
+
+if opr=="+":
+ c=a+b
+elif opr=="-":
+ c=a-b
+elif opr=="*":
+ c=a*b
+elif opr=="/":
+ c=a*b 
+elif opr=="%":
+ c=a%b 
+else:
+ print("Enter correct operator")
+
+print(c) 
+
+ 
---
+++ pythonbasics/Conditional_Statements.py
@@ -0,0 +1,13 @@
+name=["Sarath Chandran M","Sharrisa marain Hutiz","Job","Sam","Abey"]
+if "Job" in name:
+    print("Job is present")
+else:
+    print("Error")
+name.sort()
+for i in name:
+    print(i)
+i=0
+while i<len(name):
+    print(name[i])
+    i=i+1
+    
