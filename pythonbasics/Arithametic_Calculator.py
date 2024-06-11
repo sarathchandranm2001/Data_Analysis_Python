6480f870de4ab7f38d866344eea860542270a54f
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-06-11 23:09:46 +0530

    Functions

--- pythonbasics/Arithametic_Calculator.py
+++ pythonbasics/Arithametic_Calculator.py
@@ -3,15 +3,15 @@ b=int(input("Enter Second Number:"))
 opr=input("Enter Operation : (+,-,*,/,%)")
 
 if opr=="+":
- c=a+b
+    c=a+b
 elif opr=="-":
  c=a-b
 elif opr=="*":
- c=a*b
+    c=a*b
 elif opr=="/":
  c=a*b 
 elif opr=="%":
- c=a%b 
+    c=a%b 
 else:
  print("Enter correct operator")
 
