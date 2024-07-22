a12bcf89d2871a1c65ee57c173046c66067d1e2e
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-07-22 15:49:34 +0530

    NUMPY

---
+++ NUMPY/2Darray.py
@@ -0,0 +1,7 @@
+import numpy as np
+oneDarr=np.array([1,2,3,4,5])
+print(oneDarr)
+twoDarr=np.array([[1,2,3],[4,5,6]])
+print(twoDarr)
+fourDarr=np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+print(fourDarr)
---
+++ NUMPY/first.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr1=np.array([1,2,3,4,5])
+arr2=np.array([6,7,8,9,11])
+print(arr1+arr2)
+print(arr2*arr1)
